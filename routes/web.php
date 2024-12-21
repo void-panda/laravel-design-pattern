799959141be7cb74d296880662abd4b950524008
Void Panda <hamzankechil@gmail.com>
2024-12-21 16:32:00 +0800

    init project

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('posts', PostController::class);
